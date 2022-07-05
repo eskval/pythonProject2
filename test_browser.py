1c32b60aa5720af2906a22676e275896b9bc09da
eskval <108740539+eskval@users.noreply.github.com>
2022-07-05 18:15:27 +0400

    Initial commit

---
+++ test_browser.py
@@ -0,0 +1,25 @@
+import send as send
+from selenium import webdriver
+from selenium.webdriver import chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path="C:\\browdriver\chromedriver.exe")
+
+driver.get("https://moychay.ru/")
+driver.implicitly_wait(5)
+driver.maximize_window()
+
+element = driver.find_element(By.XPATH, "//input[@id='keyword']")
+element.send_keys("пуэр" + Keys.RETURN)
+driver.quit()
+
+
+
+
+
+
+
+
+
+
